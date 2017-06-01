a17d5bcdefa45ae2050e54fd3d9d116d840ff900
castor <fullmanchiu@gmail.com>
2017-06-01 11:32:21 +0800

    111

---
+++ src/com/guessmusic/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.guessmusic.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.guessmusic.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ src/com/guessmusic/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package com.guessmusic.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.guessmusic.R;
+
+public class MainActivity extends Activity {
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.main_start);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
